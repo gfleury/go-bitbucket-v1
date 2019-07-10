77866202147acf3a2bdc0f583b59a7ece526e7d9
Hugues Malphettes <hmalphettes@gmail.com>
2019-07-10 16:44:24 +0800

    Add SetRepositoryPermissionGroup(projectKey, repositorySlug, permission string, groupNames []string
    https://docs.atlassian.com/bitbucket-server/rest/5.16.0/bitbucket-rest.html#idm8297426496

--- default_api.go
+++ default_api.go
@@ -4703,7 +4703,7 @@ func (a *DefaultApiService) GetGroupsWithoutAnyPermission_14(localVarOptionals m
 }
 
 // AddRepositoryPermissionGroup https://docs.atlassian.com/bitbucket-server/rest/5.16.0/bitbucket-rest.html#idm8297426496
-func (a *DefaultApiService) SetRepositoryPermissionGroups(projectKey, repositorySlug, permission, groupNames []string, localVarHTTPContentTypes []string) (*APIResponse, error) {
+func (a *DefaultApiService) SetRepositoryPermissionGroups(projectKey, repositorySlug, permission string, groupNames []string, localVarHTTPContentTypes []string) (*APIResponse, error) {
     var (
         localVarHTTPMethod = strings.ToUpper("Put")
         localVarFileName   string
