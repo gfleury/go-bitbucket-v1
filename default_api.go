0f2067afb7ed6e07d507520bc489882aaf4a9566
Hugues Malphettes <hmalphettes@gmail.com>
2020-02-01 10:31:36 +0800

    Fix CreateSSHKey

--- default_api.go
+++ default_api.go
@@ -8453,7 +8453,9 @@ func (a *DefaultApiService) CreateSSHKey(localVarOptionals map[string]interface{
 		localVarQueryParams.Add("user", parameterToString(localVarTempParam, ""))
 	}
 	if localVarTempParam, localVarOk := localVarOptionals["text"].(string); localVarOk {
-		localVarFormParams.Add("text", parameterToString(localVarTempParam, ""))
+		localVarPostBody = map[string]interface{}{
+			"text": localVarTempParam,
+		}
 	}
 
 	// to determine the Content-Type header
