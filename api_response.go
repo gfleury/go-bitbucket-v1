89ba0cdc30dbfc4178baaa2e8bb901b73239e8ac
gfleury <gfleury@gmail.com>
2020-03-05 16:59:10 -0300

    Normalizing tests with golang 1.4

--- api_response.go
+++ api_response.go
@@ -416,7 +416,7 @@ func NewAPIResponse(r *http.Response) *APIResponse {
 // NewAPIResponseWithError create new erroneous API response from http.response and error
 func NewAPIResponseWithError(r *http.Response, err error) (*APIResponse, error) {
 
-	response := &APIResponse{Response: r, Message: err.Error()}
+	response := &APIResponse{Response: r, Message: strings.ReplaceAll(err.Error(), "\"", "")}
 	return response, err
 }
 
